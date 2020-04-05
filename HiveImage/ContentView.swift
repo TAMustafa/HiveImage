218d4cdcb10dd2b09d58575f43e5eaf8a599081c
TAMustafa <tareksnews@gmail.com>
2020-04-05 18:56:45 +0200

    Changed RGB Values

--- HiveImage/ContentView.swift
+++ HiveImage/ContentView.swift
@@ -10,27 +10,36 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var sliderValue: Double = 0.5
+    @State private var sliderValue: Double = 0.7
     
-    var redValue = Double.random(in: 0.1...1)
-    var greenValue = Double.random(in: 0.1...1)
-    var blueValue = Double.random(in: 0.1...1)
+    var redValue = Double.random(in: 0...1)
+    var greenValue = Double.random(in: 0...1)
+    var blueValue = Double.random(in: 0...1)
+    
+    var randomValue = Double.random(in: 0...10)
     
     var body: some View {
         ZStack {
-            Color(red: redValue / sliderValue, green: greenValue * sliderValue, blue: blueValue / sliderValue, opacity: 0.3)
+            Color(
+                red: redValue * sliderValue,
+                green: greenValue * sliderValue,
+                blue: blueValue * sliderValue, opacity: 0.3)
             VStack {
                 Image("Hive")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 300, height: 300, alignment: .center)
-                    .foregroundColor(Color(red: redValue * sliderValue, green: greenValue * sliderValue, blue: blueValue * sliderValue, opacity: 0.8))
+                    .foregroundColor(Color(
+                        red: redValue * (sliderValue + randomValue),
+                        green: greenValue * (sliderValue + randomValue),
+                        blue: blueValue * (sliderValue + randomValue),
+                        opacity: 0.8))
                 
                 VStack(alignment: .center) {
-                    Slider(value: $sliderValue, in: 0.3...1, step: 0.01)
-                    Text("red: \(redValue * sliderValue, specifier: "%.2f")")
-                    Text("green: \(greenValue * sliderValue, specifier: "%.2f")")
-                    Text("blue: \(blueValue * sliderValue, specifier: "%.2f")")
+                    Slider(value: $sliderValue, in: 0.2...1, step: 0.01)
+                    Text("red: \(255 * (redValue * (sliderValue + randomValue)), specifier: "%.0f")")
+                    Text("green: \(255 * (greenValue * (sliderValue + randomValue)), specifier: "%.0f")")
+                    Text("blue: \(255 * (blueValue * (sliderValue + randomValue)), specifier: "%.0f")")
                 }
                 .padding()
                 .font(.system(size: 20, weight: .bold, design: .monospaced))
